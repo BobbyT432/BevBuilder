d36a31fef4d6fc02100519e5f1d7124ba3c89ae6
Bocabowa <boleetee@gmail.com>
2023-04-21 22:49:13 -0700

    Removed unnecessary lines

--- BB/public/javascripts/form-validation.js
+++ BB/public/javascripts/form-validation.js
@@ -17,22 +17,3 @@
     }, false)
   })
 })()
-
-// // Adding ingredients
-// var counter= 4;
-
-// function add_ingred() {
-// var completelist= document.getElementById("ing-list");
-
-// completelist.innerHTML += "<li class=\"list-group-item d-flex justify-content-between lh-sm\">";
-// completelist.innerHTML += "<div>";
-// completelist.innerHTML += "<h6 class=\"my-0\">Ingredient</h6>";
-// completelist.innerHTML += "<small class=\"text-muted\">Optional / Required</small>";
-// completelist.innerHTML += "</div>";
-// completelist.innerHTML += "<span class=\"text-muted\">Amount</span>";
-// completelist.innerHTML += "</li>";
-
-
-
-// counter++;
-// }
