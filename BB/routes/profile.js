d46b95fdc4d291473e05c35b8c128488d3a4941a
kenji-adachi <49734309+kenji-adachi@users.noreply.github.com>
2023-04-23 13:55:46 -0700

    updated

--- BB/routes/profile.js
+++ BB/routes/profile.js
@@ -6,6 +6,9 @@ const User = require('../models/user');
 const Beverage = require('../models/beverage');
 const Ingredient = require('../models/ingredient');
 const BevIng = require('../models/beving');
+const UserSaveBev = require('../models/usersavebev');
+const { Sequelize } = require('sequelize');
+const Comment = require('../models/comment')
 
 const sessionChecker = (req, res, next)=> {
     if(req.session.user) {
@@ -15,15 +18,44 @@ const sessionChecker = (req, res, next)=> {
     }
 }
 
-router.use(sessionChecker)
+router.use(sessionChecker);
 
 router.get('/', async function(req, res, next) {
-    // console.log("User Session:")
-    // console.log(req.session.user)
-
-    
     const currentUser = req.session.user
-    res.render('profile', { currentUser: currentUser });
+
+    //get the list of beverages by current user
+    const myBevs = await Beverage.findAll({
+      where: {
+        author: currentUser.username
+      }
+    });
+
+    //get list of saved beverages
+    const mySavedBevIds = await UserSaveBev.findAll({
+        where: {
+          username: currentUser.username
+        }
+      });
+
+    //get each beverage information
+    tempList = []
+    for (bevId of mySavedBevIds){
+      const bevInfo = await Beverage.findByPk(bevId.bev_id)
+      tempList.push(bevInfo)
+    }
+    const mySaved = tempList
+
+    //get comments made by a user
+    const myComments = await Comment.findAll({
+      where: {
+        username: currentUser.username
+      }
+    })
+    console.log("COMMENTS")
+    console.log(myComments)
+    
+    //pass to ejs
+    res.render('profile', { currentUser: currentUser, myBevs: myBevs, mySaved: mySaved, myComments: myComments });
 });
   
 module.exports = router;
